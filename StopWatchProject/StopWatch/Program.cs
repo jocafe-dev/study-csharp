fd4be024bbac2c6e04575a053c5766e757b1fcb0
Jônatas Carvalho <jocafe.dev@gmail.com>
2023-02-06 19:48:37 -0300

    Add Stop Watch project v0.1

---
+++ StopWatchProject/StopWatch/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = segundo => 10s = 10 segundos");
+            Console.WriteLine("M = minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char.TryParse(data.Substring(data.Length - 1, 1), out char type);
+            int.TryParse(data.Substring(0, data.Length - 1), out int time);
+            int multiplier = 1;
+
+            if (type == 'm') multiplier = 60;
+            if (time == 0) System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
